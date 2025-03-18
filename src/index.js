52ad5f6874f57ca75e824bb24ba6d2bcece904f7
Vishal Shingala <vishalshingala.co23d2@scet.ac.in>
2025-03-18 15:29:05 +0530

    Util code added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 })
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at ${process.env.PORT} port`);
+        
+    });
+})
+.catch((err)=>{
+    console.log("MONGODB CONNECTIOIN FAILED: ", err);
+    
+})
